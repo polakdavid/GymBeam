93a1bf9ccbde321454899bb94d5d23b022b5025f
polakdavid <polak-david@hotmail.com>
2023-11-02 09:22:35 +0100

    Add files via upload

---
+++ etc/addons/case_study/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import hr_employee_base
+from . import hr_employee
+from . import hr_employee_category
+from . import hr_employee_public
+from . import res_partner
+from . import res_users
